710ea31a6ef0fa79b483f5bc62261c97ccee592b
Marc Donald <dev@marcdonald.com>
2024-04-20 00:02:59 +0100

    Add import logic + frontmatter population

--- src/import-json.ts
+++ src/import-json.ts
@@ -1,10 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Vault } from 'obsidian';
+import { moment, Vault } from 'obsidian';
 import { DayOneImporterSettings } from './main';
-
-function sleep(milliseconds: number) {
-	return new Promise((resolve) => setTimeout(resolve, milliseconds));
-}
+import * as path from 'node:path';
 
 export async function importJson(
 	vault: Vault,
@@ -25,14 +22,62 @@ export async function importJson(
 		console.log(`Found ${entries.length} journal entries`);
 
 		let successCount = 0;
-		const failureCount = 0;
-		entries.forEach((item: any, i: number) => {
-			if (i < 3) {
-				successCount++;
-				vault.create(
-					settings.outDirectory + '/' + item.creationDate + '.md',
-					item.text
+		let failureCount = 0;
+		entries.forEach((item: any) => {
+			try {
+				const fileName =
+					moment(item.creationDate).format(`YYYYMMDDHHMMss`) + '.md';
+
+				let fileData = '---\n';
+				fileData += buildFrontmatterProperty(
+					'creationDate',
+					`${moment(item.creationDate).format('YYYY-MM-DD')}T${moment(item.creationDate).format('HH:MM')}`
 				);
+				fileData += buildFrontmatterProperty(
+					'modifiedDate',
+					`${moment(item.modifiedDate).format('YYYY-MM-DD')}T${moment(item.modifiedDate).format('HH:MM')}`
+				);
+
+				if (item.isAllDay) {
+					fileData += buildFrontmatterProperty('isAllDay', 'true');
+				}
+				if (item.isPinned) {
+					fileData += buildFrontmatterProperty('isPinned', 'true');
+				}
+				if (item.starred) {
+					fileData += buildFrontmatterProperty('starred', 'true');
+				}
+
+				if (item.tags) {
+					fileData += buildFrontmatterProperty('tags', item.tags);
+				}
+
+				if (item.userActivity) {
+					fileData += buildFrontmatterProperty(
+						'activity',
+						item.userActivity.activityName
+					);
+				}
+				if (item.location) {
+					fileData += buildFrontmatterProperty(
+						'location',
+						`${item.location.placeName}, ${item.location.localityName}, ${item.location.country}`
+					);
+				}
+
+				fileData += `---\n`;
+
+				fileData += `${item.text}`;
+
+				vault.create(path.join(settings.outDirectory, fileName), fileData, {
+					ctime: new Date(item.creationDate).getTime(),
+					mtime: new Date(item.modifiedDate).getTime(),
+				});
+				console.log(`${fileName} created successfully`);
+				successCount++;
+			} catch (e) {
+				console.error(e);
+				failureCount++;
 			}
 		});
 
@@ -45,4 +90,15 @@ export async function importJson(
 		console.error(err);
 		throw err;
 	}
+}
+
+function buildFrontmatterProperty(
+	propertyName: string,
+	propertyValue: string | string[]
+) {
+	if (Array.isArray(propertyValue)) {
+		return `${propertyName}:\n${propertyValue.map((item: string) => '  - ' + item + '\n').join('')}\n`;
+	}
+
+	return `${propertyName}: ${propertyValue}\n`;
 }
